182981f885eb9a38025a499f8633ae15fe0a1727
Luca Garizio <garizioluca2@gmail.com>
2023-12-01 15:28:36 +0100

    feat: transform string in img

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ const employees = [
     {
         "name": "Angela Caroll",
         "role": "Chief Editor",
-        "image": "angela-caroll-chief.editor.jpg"
+        "image": "angela-caroll-chief-editor.jpg"
     },
     {
         "name": "Walter Gordon",
         "role": "Office Manager",
-        "image": "walter-gordon-office-editor.jpg"
+        "image": "walter-gordon-office-manager.jpg"
     },
     {
         "name": "Angela Lopez",
@@ -41,18 +41,24 @@ const employees = [
 for (let i = 0; i < employees.length; i++){
     let indexArray = employees[i];
     // console.log(indexArray);
-    
+
     for (let attributeVal in indexArray) {
         console.log(attributeVal,indexArray[attributeVal]);
+        const printMembers = document.getElementById("team");
+        const memberImage = document.getElementById("immagini");
+        // MILESTONE 2:
+        // Stampare le stesse informazioni su DOM sotto forma di stringhe
+        printMembers.innerHTML += `<h2> ${attributeVal} : ${indexArray[attributeVal]} </h2>`;
+
+        // BONUS 1:
+        // Trasformare la stringa foto in una immagine effettiva
+        if ( attributeVal === "image") {
+        memberImage.innerHTML += `<img src="${indexArray['image']}">`;
+        }
     }
 };
 
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sotto forma di stringhe
 
-
-// BONUS 1:
-// Trasformare la stringa foto in una immagine effettiva
 
 // BONUS 2:
 // Organizzare i singoli membri in card/schede
