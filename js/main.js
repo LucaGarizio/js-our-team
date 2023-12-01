cc8aa0457c29269607d79324aefef8e85554613c
Luca Garizio <garizioluca2@gmail.com>
2023-12-01 14:26:16 +0100

    feat: add object & for cycle

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const employees = [
+    {
+        "name": "Wayne Barnet",
+        "role": "Founder & CEO",
+        "image": "wayne-barnet-founder-ceo.jpg"
+    },
+    {
+        "name": "Angela Caroll",
+        "role": "Chief Editor",
+        "image": "angela-caroll-chief.editor.jpg"
+    },
+    {
+        "name": "Walter Gordon",
+        "role": "Office Manager",
+        "image": "walter-gordon-office-editor.jpg"
+    },
+    {
+        "name": "Angela Lopez",
+        "role": "Social Media Manager",
+        "image": "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "name": "Scott Estrada",
+        "role": "Developer",
+        "image": "scott-estrada-developer.jpg"
+    },
+    {
+        "name": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "image": "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for (let i = 0; i < employees.length; i++){
+    let indexArray = employees[i];
+    // console.log(indexArray);
+    
+    for (let attributeVal in indexArray) {
+        console.log(attributeVal,indexArray[attributeVal]);
+    }
+};
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sotto forma di stringhe
+
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+// Consigli del giorno:
+// Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice.
+// E ricordiamoci che console.log() è nostro amico!
